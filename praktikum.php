2ad23ec6556188f14525d1b1c551ef674bef0dea
Hafizh <110800141+MuhammadHafizhRaharja@users.noreply.github.com>
2025-03-27 13:54:35 +0700

    Delete praktikum.php

--- praktikum.php
+++
@@ -1,7 +0,0 @@
-<?php
-$bukuBuku = [
-    ["judul" => "Pemrograman Web", "harga" => 50000, "terjual" => 3],
-    ["judul" => "Belajar Javascript", "harga" => 60000, "terjual" => 2],
-    ["judul" => "HTML & CSS", "harga" => 40000, "terjual" => 5],
-];
-?>
